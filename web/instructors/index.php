d665f2fe05da7c7d27341a9328b9ea513f49551f
Arguggi <git@arguggi.co.uk>
2015-05-15 21:01:00 +0200

    Add instructors page

---
+++ web/instructors/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+  <title></title>
+</head>
+<body>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <div class="content">
+    <div class="container">
+      <h2 class="center-text">All Instructors</h2>
+      <div class="row">
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+      </div>
+    <div class="row">
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+        <div class="col-md-4"><img class="instruc-img img-rounded" src="/assets/instructor.jpg"></div>
+      </div>
+    </div>
+  </div>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+</body>
+</html>
