8a1ee9288f7398800ac72eba42f76dfd9aa11883
Arguggi <git@arguggi.co.uk>
2015-05-15 17:48:50 +0200

    Start location page

---
+++ web/rooms/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+  <title></title>
+</head>
+<body>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <div class="content">
+  </div>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+</body>
+</html>
