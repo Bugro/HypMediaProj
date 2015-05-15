28572fa30c5a5cd59196f6d08eabda0005de63e5
Arguggi <git@arguggi.co.uk>
2015-05-15 08:42:44 +0200

    Switch to php includes
    I'm using php just to copy the common head, header, footer files in
    each index file so we only have to change one file and every other file
    will be fixed automatically.
    
    You WILL have to use a server to view the page now, opening the
    index.php with a browser will not work.
    
    You should already have php installed, you can open a terminal,
        change directory to the web folder and then write:
    
    php -S localhost:8000
    
    and if you the visit localhost:8000 in your browser it should work.
    Otherwise copy the web folder in the xampp folder and it should work.

---
+++ web/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+  <title></title>
+</head>
+<body>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <div class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4"><img class="body-img" src="/assets/1.jpg"></div>
+        <div class="col-md-4"><img class="body-img" src="/assets/2.jpg"></div>
+        <div class="col-md-4"><img class="body-img" src="/assets/equipment.jpg"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-4"><img class="body-img" src="/assets/location.jpg"></div>
+        <div class="col-md-4"><img class="body-img" src="/assets/testimonials.jpg"></div>
+        <div class="col-md-4"><img class="body-img" src="/assets/6.png"></div>
+      </div>
+    </div>
+  </div>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+</body>
+</html>
