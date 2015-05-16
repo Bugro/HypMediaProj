ab2487f4a4d94f1e00aafa919fd74914b809e9f6
Arguggi <git@arguggi.co.uk>
2015-05-16 13:04:34 +0200

    Add Gym page

--- web/gym/index.php
+++ web/gym/index.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
   <title></title>
 </head>
 <body>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
   <div class="content">
+    <div class="container">
+      <h2 class="center-text">Welcome to the Gym</h2>
+<img class="center-img img-rounded" src="/assets/gym.jpg"> <a href="/history/" class="btn btn-primary btn-room" role="button">History</a>
+      <div class="row">
+        <div class="col-md-4">
+          <a href="/location/" class="btn btn-primary btn-room" role="button">Location</a>
+        </div>
+        <div class="col-md-4">
+          <a href="/testimonials/" class="btn btn-primary btn-room" role="button">Testimonials</a>
+        </div>
+        <div class="col-md-4">
+          <a href="/Equipment/" class="btn btn-primary btn-room" role="button">Equipment</a>
+        </div>
+      </div>
+    </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
   </div>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
 </body>
 </html>
