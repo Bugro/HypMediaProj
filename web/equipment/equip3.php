6107c580a926b7a9d559978c3bf5f38358d0775e
Arguggi <git@arguggi.co.uk>
2015-05-16 16:35:15 +0200

    Fix room - equipment confusion

--- web/equipment/equip3.php
+++ web/equipment/equip3.php
@@ -7,16 +7,17 @@
   <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
   <div class="content">
     <div class="container">
-      <h2 class="center-text">Endurance B3R Recumbent Bike</h2><img class="center-img img-rounded img-equip" src="/assets/equip-3.jpg">
+      <h2 class="center-text">Our Equipment</h2>
+      <h3 class="center-text">Endurance B3R Recumbent Bike</h3><img class="center-img img-rounded img-equip" src="/assets/equip-3.jpg">
       <p>The Endurance ® B3R Recumbent Bike gives you a cardiovascular workout without straining your body. It features 16 levels of resistance so you can find a workout that fits your needs. Self-generating electronics provide feedback during your workout. Contact heart rate and advanced heart rate control lets you monitor your workout. Dura-Firm ™ seat padding makes your ride comfortable and provides lumbar support.</p>
       <div class="row">
         <div class="col-md-6">
-          <a href="/equipment/equip2.php" class="btn btn-primary btn-room" role="button">Previous Room</a>
+          <a href="/equipment/equip2.php" class="btn btn-primary btn-room" role="button">Previous Machine</a>
         </div>
         <div class="col-md-6">
-          <a href="" class="btn btn-default btn-room" role="button">Next Room</a>
+          <a href="" class="btn btn-default btn-room" role="button">Next Machine</a>
         </div>
-      </div>
+      </div><a href="/rooms/" class="btn btn-primary btn-room" role="button">Our rooms</a>
     </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
   </div>
 </body>
--- web/equipment/index.php
+++ web/equipment/index.php
@@ -7,16 +7,17 @@
   <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
   <div class="content">
     <div class="container">
-      <h2 class="center-text">SOLE E95 Elliptical</h2><img class="center-img img-rounded img-equip" src="/assets/equip-1.jpg">
+      <h2 class="center-text">Our Equipment</h2>
+      <h3 class="center-text">SOLE E95 Elliptical</h3><img class="center-img img-rounded img-equip" src="/assets/equip-1.jpg">
       <p>The SOLE E95 Elliptical is equipped with all you need to reach your peak performance. Features include an easy-to-use electronic console with a vibrant 9-inch LCD display to monitor your progress and keep track of information including speed, distance, heart rate, and much more. Built with a whisper-quiet drive system and 34 lb flywheel, the SOLE E95 allows both forward and reverse motion so you can target specific muscles and tone your body.</p>
       <div class="row">
         <div class="col-md-6">
-          <a href="" class="btn btn-default btn-room" role="button">Previous Room</a>
+          <a href="" class="btn btn-default btn-room" role="button">Previous machine</a>
         </div>
         <div class="col-md-6">
-          <a href="/equipment/equip2.php" class="btn btn-primary btn-room" role="button">Next Room</a>
+          <a href="/equipment/equip2.php" class="btn btn-primary btn-room" role="button">Next machine</a>
         </div>
-      </div>
+      </div><a href="/rooms/" class="btn btn-primary btn-room" role="button">Our rooms</a>
     </div>
   </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
 </body>
