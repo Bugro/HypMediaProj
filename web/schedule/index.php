7c2df54d6e84cfc20ef40d9fa30039034f9672e8
Arguggi <git@arguggi.co.uk>
2015-05-17 00:04:42 +0200

    Add schedule page

--- web/schedule/index.php
+++ web/schedule/index.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
-  <title></title>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
 </head>
 <body>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
   <div class="content">
-  </div>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+    <div class="container">
+      <h1 class="center-text">Schedule</h1>
+      <h3 class="center-text">Monday</h3>
+      <p class="center-text">07:00 - 22:00</p>
+      <h3 class="center-text">Tuesday</h3>
+      <p class="center-text">09:00 - 22:00</p>
+      <h3 class="center-text">Wednesday</h3>
+      <p class="center-text">07:00 - 22:00</p>
+      <h3 class="center-text">Thursday</h3>
+      <p class="center-text">09:00 - 22:00</p>
+      <h3 class="center-text">Friday</h3>
+      <p class="center-text">07:00 - 22:00</p>
+      <h3 class="center-text">Saturday</h3>
+      <p class="center-text">10:00 - 18:00</p>
+      <h3 class="center-text">Sunday</h3>
+      <p class="center-text">10:00 - 13:00</p>
+    </div>
+  </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
 </body>
 </html>
