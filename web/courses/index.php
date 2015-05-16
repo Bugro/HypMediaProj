d5826834c364ed331a3c23074c22ff4df7590135
Arguggi <git@arguggi.co.uk>
2015-05-16 23:55:02 +0200

    Add courses page

---
+++ web/courses/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+</head>
+<body>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <div class="content">
+    <div class="container">
+      <h1 class="center-text">Courses</h1>
+      <div class="row bottom-padding">
+        <div class="col-md-4">
+          <a href="/courses/" class="btn btn-success btn-room" role="button">All courses - Alphabetically</a>
+        </div>
+        <div class="col-md-4">
+          <a href="/courses/" class="btn btn-primary btn-room" role="button">All courses - By Level</a>
+        </div>
+        <div class="col-md-4">
+          <a href="/courses/" class="btn btn-primary btn-room" role="button">All courses - By Category</a>
+        </div>
+      </div><a href="/courses/">
+      <h2 class="center-text">Circuit Tone</h2></a> <a href="/courses/">
+      <h2 class="center-text">Interval Training</h2></a> <a href="/courses/">
+      <h2 class="center-text">Muay Thai</h2></a> <a href="/courses/">
+      <h2 class="center-text">Taek Won Do</h2></a> <a href="/courses/">
+      <h2 class="center-text">Total Body</h2></a> <a href="/courses/">
+      <h2 class="center-text">TRX Training</h2></a>
+    </div>
+  </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+</body>
+</html>
