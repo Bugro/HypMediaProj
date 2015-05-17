5ccd1eafed386de40ae154f28f8c04825a67eef6
Arguggi <git@arguggi.co.uk>
2015-05-17 20:36:58 +0200

    Add schedule headers

--- web/schedule/index.php
+++ web/schedule/index.php
@@ -8,6 +8,16 @@
   <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
   <div class="content">
     <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h3 class="center-text green-text">BIG GYM SCHEDULE</h3>
+          <h3 class="center-text green-text">JUNE - AUGUST 2015</h3>
+        </div>
+        <div class="col-md-6">
+          <h3 class="center-text green-text">- Opening Hours -</h3>
+          <h3 class="center-text green-text">07:00 a.m. - 11:00 p.m.</h3>
+        </div>
+      </div>
       <div id="calendar" class="fc-calendar-container"></div>
       <script type="text/javascript" src="/js/jquery.calendario.js">
       </script>
