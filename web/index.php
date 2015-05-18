01cb7e61de7c4676d45bfeb8cf7020d5ca3ed356
Arguggi <git@arguggi.co.uk>
2015-05-18 16:07:19 +0200

    Add text overlay to images

--- web/index.php
+++ web/index.php
@@ -1,25 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
-  <title></title>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
 </head>
 <body>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
   <div class="content">
     <div class="container">
       <div class="row">
-        <div class="col-md-4"><img class="body-img img-rounded" src="/assets/1.jpg"></div>
-        <div class="col-md-4"><img class="body-img img-rounded" src="/assets/2.jpg"></div>
-        <div class="col-md-4"><img class="body-img img-rounded" src="/assets/equipment.jpg"></div>
+        <div class="col-md-4">
+          <div class="overlay-img">
+            <img class="body-img img-rounded" src="/assets/1.jpg">
+            <p class="desc">OUR INSTRUCTORS</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="overlay-img">
+            <img class="body-img img-rounded" src="/assets/2.jpg">
+            <p class="desc">OUR EQUIPMENT</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="overlay-img">
+            <img class="body-img img-rounded" src="/assets/equipment.jpg">
+            <p class="desc">FITNESS ROOMS</p>
+          </div>
+        </div>
       </div>
       <div class="row">
-        <div class="col-md-4"><img class="body-img img-rounded" src="/assets/location.jpg"></div>
-        <div class="col-md-4"><img class="body-img img-rounded" src="/assets/testimonials.jpg"></div>
-        <div class="col-md-4"><img class="body-img img-rounded" src="/assets/6.png"></div>
+        <div class="col-md-4">
+          <div class="overlay-img">
+            <img class="body-img img-rounded" src="/assets/location.jpg">
+            <p class="desc">LOCATION</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="overlay-img">
+            <img class="body-img img-rounded" src="/assets/testimonials.jpg">
+            <p class="desc">TESTIMONIALS</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="overlay-img">
+            <img class="body-img img-rounded" src="/assets/6.png">
+            <p class="desc">REGISTER</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+  </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
 </body>
 </html>
