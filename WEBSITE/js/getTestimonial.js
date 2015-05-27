70c77997179b9fd991b58311ad99112ccd73f6ba
Arguggi <git@arguggi.co.uk>
2015-05-27 17:34:04 +0200

    Remove console.log used for debugging

--- WEBSITE/js/getTestimonial.js
+++ WEBSITE/js/getTestimonial.js
@@ -1,13 +1,10 @@
 $( document ).ready(function() {
 
     getDBTable("testimony", function(testimonial) {
-            console.log("callback");
-            console.log(testimonial);
         if (typeof testimonial == 'string' || testimonial instanceof String) {
             console.log("Error while getting database data");
         } else {
             var testimonialList="";
-            console.log("what?");
             for (var i=0; i < testimonial.length; i++){
                 $("#testimonialList").append("<div class='col-md-6'><p class='gray-border'>&quot;"+testimonial[i].quotation+"&quot; - "+testimonial[i].name+"</p></div>");
             }
