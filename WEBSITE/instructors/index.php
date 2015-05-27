dda11bfabc0b898ca0a82a9a124e0cada076ead6
Bugro <guillaume.burgaud@free.fr>
2015-05-28 01:51:37 +0200

    Instructor: Done Of course it's not totally finished but now it's linked to DB

--- WEBSITE/instructors/index.php
+++ WEBSITE/instructors/index.php
@@ -5,51 +5,10 @@
 </head>
 <body>
   <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+    <script type="text/javascript" src="/js/getInstructor.js"></script>
   <div class="content">
-    <div class="container">
-      <h2 class="center-text">Personal Fitness Trainers</h2>
-      <div class="row">
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-amy.jpg">
-            <p class="desc white-links">AMY</p>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-katie.jpg">
-            <p class="desc white-links">KATIE</p>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-mike.jpg">
-            <p class="desc white-links">MIKE</p>
-          </div>
-        </div>
-      </div>
-      <h2 class="center-text">Boxing/Kickbox Trainer</h2>
-      <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-adam.jpg">
-            <p class="desc white-links">ADAM</p>
-          </div>
-        </div>
-        <div class="col-md-4"></div>
-      </div>
-      <h2 class="center-text">Pilates / Yoga Trainer</h2>
-      <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-mia.jpg">
-            <p class="desc white-links">MIA</p>
-          </div>
-        </div>
-        <div class="col-md-4"></div>
-      </div>
+    <div class="container" id="instructorList">
+      
     </div>
   </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
 </body>
---
+++ WEBSITE/js/getInstructor.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+    getDBTable("instructor", function(instructor) {
+        if (typeof instructor == 'string' || instructor instanceof String) {
+            console.log("Error while getting database data");
+        } else {
+            var instructorList="";
+            for(var i=0;i<instructor.length;i++){
+                $("#instructorList").append("<div id='"+instructor[i].name.split(' ')[0]+"'><div class='col-md-4'><div class='small-overlay-img'><img class='small-img img-rounded' src='/assets/instruc-"+instructor[i].name+".jpg'><p class='desc white-links'>"+instructor[i].name+"</p></div></div></div>");
+               $("#"+instructor[i].name.split(' ')[0]+"").on("click", {title: instructor[i].name, description: instructor[i].description, speciality: instructor[i].speciality}, displayDescription);
+            }
+        }
+    });
+});
+
+
+function displayDescription(event) {
+    elmt = "<h3 class='center-text well black-links' id='instructorTitle'>"+event.data.title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='/assets/instruc-"+event.data.title+".jpg'></div></div><div class='col-md-9' id='instructorDescription'><p>"+event.data.description+"</p><p><h2 class='well black-links'>Speciality: "+event.data.speciality+"</h2></p></div></div><a href='/instructors/' class='btn btn-primary btn-room' role='button'>Back</a>";
+    $("#instructorList").html(elmt);
+}
