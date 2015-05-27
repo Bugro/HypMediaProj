f87bd1801af6cb77841cec92fe75b2e3731c13a9
Arguggi <git@arguggi.co.uk>
2015-05-27 15:26:15 +0200

    Remove html from files
    Since we are getting the data from the DB we don't need it in the .html
    file.

--- WEBSITE/courses/index.php
+++ WEBSITE/courses/index.php
@@ -13,46 +13,6 @@
           <a href="/courses/" class="btn btn-primary btn-room" role="button">All courses - Alphabetically</a> <a href="/courses/" class="btn btn-success btn-room" role="button">All courses - By Level</a> <a href="/courses/" class="btn btn-success btn-room" role="button">All courses - By Category</a>
         </div>
         <div class="col-md-9" id="courseList">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="small-overlay-img">
-                <a href="/courses/boxing.php"><img class="small-img img-rounded" src="/assets/course-boxing.jpg">
-                <p class="desc white-links">BOXING</p></a>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="small-overlay-img">
-                <a href="/courses/hathayoga.php"><img class="small-img img-rounded" src="/assets/course-hatha.jpg">
-                <p class="desc white-links">HATHA YOGA</p></a>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="small-overlay-img">
-                <a href="/courses/kickboxing.php"><img class="small-img img-rounded" src="/assets/course-kickboxing.jpg">
-                <p class="desc white-links">KICKBOXING</p></a>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-4">
-              <div class="small-overlay-img">
-                <a href="/courses/muaythai.php"><img class="small-img img-rounded" src="/assets/course-muaythai.jpg">
-                <p class="desc white-links">MUAY THAI</p></a>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="small-overlay-img">
-                <a href="/courses/pilates.php"><img class="small-img img-rounded" src="/assets/course-pilates.jpg">
-                <p class="desc white-links">PILATES</p></a>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="small-overlay-img">
-                <a href="/courses/trxtraining.php"><img class="small-img img-rounded" src="/assets/course-trxtraining.jpg">
-                <p class="desc white-links">TRX TRAINING</p></a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
--- WEBSITE/testimonials/index.php
+++ WEBSITE/testimonials/index.php
@@ -13,15 +13,6 @@
       <h2 class="center-text">Our valuable members thoughts about us:</h2>
 
       <div class="row" id="testimonialList">
-        <div class="col-md-6">
-          <p class="gray-border">"I have never looked forward to going to a gym until I joined BIG - and my enthusiasm is beginning to show in my physique!" –Barbara Yales</p>
-          <p class="gray-border">"It’s encouraging to see people at all fitness levels working out under the same roof. Plus, BIG is in my backyard, there is no excuse not to go!"  -Chris Hummel</p>
-          <p class="gray-border">"Not your typical, boring gym!... I have never been to a gym with such attentive staff, it truly makes a difference for me.” –Adela McCon</p>
-        </div>
-        <div class="col-md-6">
-          <p class="gray-border">"Within 6 short months I’m now in the best shape of my life and I’m even wearing the same size pants I wore back in high school.  Thanks BIG – I couldn’t have done it without your help!” -Pete Finburgh</p>
-          <p class="gray-border">"BIG has completely changed the way I exercise. The classes are great. The trainers and staff are super helpful. Plus the people I've met at the gym are amazing; I've made a ton of friends since I joined over five years ago. You can't beat the atmosphere; it's the best gym in the city." –David Stein</p>
-        </div>
       </div>
       <h2 class="center-text">You can add a testimonial via social media by using #BIGFitnessClub #BIGGym hasthtags.</h2>
       <h2 class="center-text">You can also find us on Facebook, Twitter and Instagram. </h2>
