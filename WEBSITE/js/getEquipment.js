83d155a515f4c9e13e99f8f5675092e2e5aff84b
Arguggi <git@arguggi.co.uk>
2015-05-28 08:42:07 +0200

    Equipement corrected to equipment
    You will have to change your local database so that the table name is
    equipment and not equipement. I already changed the common mysql
    database.

---
+++ WEBSITE/js/getEquipment.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+    getDBTable("equipment", function(equipment) {
+        if (typeof equipment == 'string' || equipment instanceof String) {
+            console.log("Error while getting database data");
+        } else {
+            var equipmentList="";
+            for(var i=0;i<equipment.length;i++){
+                $("#equipmentList").append("<h3 class='center-text col-md-12'>"+equipment[i].title+"</h3><div class='row'><div class='small-overlay-img'><div class='col-md-3'><img class='img-equip center-img img-rounded' src='/assets/equip-"+equipment[i].id+".jpg'></div></div><div class='col-md-9'><p>"+equipment[i].descriprion+"</p></div></div>");
+            }
+        }
+    });
+});
