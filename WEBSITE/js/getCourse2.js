55bd934695cc8dd9853c5094dc2d1545eb785c05
Arguggi <git@arguggi.co.uk>
2015-05-28 08:43:30 +0200

    Delete usused files
    It you want to see unused files we deleted from the repo:
    https://stackoverflow.com/questions/338436/is-there-a-quick-git-command-to-see-an-old-version-of-a-file
    https://stackoverflow.com/questions/278192/view-the-change-history-of-a-file-using-git-versioning

--- WEBSITE/js/getCourse2.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    $( ".beep" ).append( "<p id='bite'>Test</p>" )
-    $("#bite").on("click", displayDescription);
-});
-
-function displayDescription(){
-    $(this).html("");
-}
