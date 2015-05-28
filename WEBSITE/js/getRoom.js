c6f7d13f4f43c1cb850e5a2ad2423de1129482c9
Bugro <guillaume.burgaud@free.fr>
2015-05-28 02:00:39 +0200

    getRoom.js improve (it's hidden but it's cleaner in term of code)

--- WEBSITE/js/getRoom.js
+++ WEBSITE/js/getRoom.js
@@ -15,15 +15,8 @@ $( document ).ready(function() {
 
 
 function displayDescription(event) {
-    elmt = "<h3 class='center-text' id='roomTitle'>"+event.data.title+"</h3><div class='row'><div class='small-overlay-img'><img class='col-md-12 img-rounded' src='/assets/"+event.data.title+".jpg'></div><div class='col-md-12' id='roomDescription'><p class='center-text'>"+event.data.description+"</p></div></div>";
+    $(".contentDisplayed").html("");
+    elmt = "<div class='contentDisplayed'><h3 class='center-text' id='roomTitle'>"+event.data.title+"</h3><div class='row'><div class='small-overlay-img'><img class='col-md-12 img-rounded' src='/assets/"+event.data.title+".jpg'></div><div class='col-md-12' id='roomDescription'><p class='center-text'>"+event.data.description+"</p></div></div></div>";
     $(this).find(".roomDisplayDesc").html(elmt);
-    $(this).on("click", {title: event.data.title, description: event.data.description}, closeDescription);
     $("#mapOfTheGym").html("<img id='imgmap' class='img-rounded' src='/assets/gymplan/gymplan"+event.data.title.split(' ')[0]+".jpg'>");
-}
-
-function closeDescription(event) {
-    elmt = "<div id='"+event.data.title+"'><div class='btn btn-primary btn-room' role='button'>"+event.data.title+"</div><div class='roomDisplayDesc'></div></div>";
-    $(this).html(elmt);
-    $(this).on("click", {title: event.data.title, description: event.data.description}, displayDescription);
-    $("#mapOfTheGym").html("<img id='imgmap' class='img-rounded' src='/assets/gymplan/gymplan.jpg'>");
 }
