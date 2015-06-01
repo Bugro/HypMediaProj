f94449c8adfd84316b3052a14094b4ae90b57fa1
Arguggi <git@arguggi.co.uk>
2015-06-01 10:58:20 +0200

    Add php table check

--- WEBSITE/php/getDB.php
+++ WEBSITE/php/getDB.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "biggym");
+$mysqli = new mysqli("localhost", "root", "mN1Oq6kNINtnypVcpTPjWFj2vVaYg18e", "biggym");
 //$mysqli = new mysqli(getenv("mysqli_default_host"), getenv("mysqli_default_user"),getenv("mysqli_default_pw"),getenv("mysqli_default_table"));
 
 if (mysqli_connect_errno()) {
@@ -7,8 +7,15 @@ if (mysqli_connect_errno()) {
     exit();
 }
 else {
-    $table = $_POST['table'];
-    $order = $_POST['order'];
+    $table_names = array("course","equipment","instructor","room","testimony");
+    if (isset($_POST['table']) && in_array($_POST['table'], $table_names, TRUE)) {
+        $table = $_POST['table'];
+    } else {
+        echo "No table name provided";
+        exit();
+    }
+    $order = (int) $_POST['order'];
+
     $query = " SELECT * FROM $table ORDER BY $order";
     $result = $mysqli->query($query);
     if ($result->num_rows > 0) {
