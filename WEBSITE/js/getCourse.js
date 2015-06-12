302aac7b41373f1df2532aee4ad9c97baeec245b
Bugro <guillaume.burgaud@free.fr>
2015-06-12 12:29:11 +0200

    Images added and modification of getCourse and getInstructor

--- WEBSITE/js/getCourse.js
+++ WEBSITE/js/getCourse.js
@@ -9,8 +9,31 @@ $( document ).ready(function() {
 
 
 function displayDescription(event) {
-    elmt = "<h3 class='center-text' id='courseTitle'>"+event.data.title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='/assets/"+event.data.title+".jpg'></div></div><div class='col-md-9' id='courseDescription'><p>"+event.data.description+"</p></div></div><a href='/courses/' class='btn btn-primary btn-room' role='button'>Back</a>";
-    $("#courseList").html(elmt);
+    allInstru = [];
+    instruList = "";
+    getDBTable("instructor", 2, function(instructor) {
+     if (typeof instructor == 'string' || instructor instanceof String) {
+            console.log("Error while getting database data");
+        } else {
+            var instructorList="";
+            for(var i=0;i<instructor.length;i++){
+                for(var k=0;k<instructor[i].speciality.length;k++){
+                    if(instructor[i].speciality.split(', ')[k] == event.data.title){
+
+                        allInstru[i] = instructor[i].name;
+                        instruList += "<div class='col-sm-4'><img class='small-img img-rounded' src='/assets/instruc-"+instructor[i].name+".jpg'></div>";
+                    }
+                }
+            }
+            $("#courseList").html("");
+    
+    elmt = "<h3 class='center-text' id='courseTitle'>"+event.data.title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='/assets/"+event.data.title+".jpg'></div></div><div class='col-md-9' id='courseDescription'><p>"+event.data.description+"</p></div></div><div id='instructorsForThisCourse'><div class='well black-links'><h2 class='center-text'>Instructors for this course</h2></div>"+instruList+"</div><a href='/courses/' class='btn btn-primary btn-room col-xs-12' role='button'>Back</a>";
+    $("#courseList").append(elmt);
+        }
+    });
+    
+    
+    
 }
 
 function displayCourseByAlpha(){
--- WEBSITE/js/getInstructor.js
+++ WEBSITE/js/getInstructor.js
@@ -8,6 +8,7 @@ $( document ).ready(function() {
             for(var i=0;i<instructor.length;i++){
                 $("#instructorList").append("<div id='"+instructor[i].name.split(' ')[0]+"'><div class='col-md-4'><div class='small-overlay-img'><img class='small-img img-rounded' src='/assets/instruc-"+instructor[i].name+".jpg'><p class='desc white-links'>"+instructor[i].name+"</p></div></div></div>");
                $("#"+instructor[i].name.split(' ')[0]+"").on("click", {title: instructor[i].name, description: instructor[i].description, speciality: instructor[i].speciality}, displayDescription);
+                console.log(instructor[i].speciality);
             }
         }
     });
@@ -15,6 +16,33 @@ $( document ).ready(function() {
 
 
 function displayDescription(event) {
-    elmt = "<h3 class='center-text well black-links' id='instructorTitle'>"+event.data.title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='/assets/instruc-"+event.data.title+".jpg'></div></div><div class='col-md-9' id='instructorDescription'><p>"+event.data.description+"</p><p><h2 class='well black-links'>Speciality: "+event.data.speciality+"</h2></p></div></div><a href='/instructors/' class='btn btn-primary btn-room' role='button'>Back</a>";
-    $("#instructorList").html(elmt);
+    $("#instructorList").html("");
+    allSpecialities = "";
+    specialityList = event.data.speciality.split(", ");
+    console.log(specialityList);
+    for(var i=0;i<specialityList.length;i++){
+        allSpecialities += "<span id='"+specialityList[i].split(' ')[0]+"'>"+specialityList[i]+" "+"</span>";
+    }
+    elmt = "<h3 class='center-text well black-links' id='instructorTitle'>"+event.data.title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='/assets/instruc-"+event.data.title+".jpg'></div></div><div class='col-md-9' id='instructorDescription'><p>"+event.data.description+"</p><p class='well black-links'>Speciality: "+allSpecialities+"</p></div></div><a href='/instructors/' class='btn btn-primary btn-room' role='button'>Back</a>";
+    $("#instructorList").append(elmt);
+    for(var i=0;i<specialityList.length;i++){
+        $("#"+specialityList[i].split(' ')[0]+"").on("click", {title: specialityList[i]}, displayCourse);
+    }
+}
+
+function displayCourse(event) {
+    $("#instructorList").html("");
+    getDBTable("course", 2, function(course) {
+        if (typeof course == 'string' || course instanceof String) {
+            console.log("Error while getting database data");
+        } else {
+            var courseList="";
+            for(var i=0;i<course.length;i++){
+                if(course[i].title == event.data.title){
+                    elmt = "<h3 class='center-text' id='courseTitle'>"+course[i].title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='/assets/"+course[i].title+".jpg'></div></div><div class='col-md-9' id='courseDescription'><p>"+course[i].description+"</p></div></div><a href='/instructors/' class='btn btn-primary btn-room' role='button'>Back</a>";
+        $("#instructorList").append(elmt);
+                }
+            }
+        }
+    });
 }
