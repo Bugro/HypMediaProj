849649598b5010abbf7ea85870094b3e60037273
Arguggi <git@arguggi.co.uk>
2015-06-12 19:31:25 +0200

    Fix getDBElement url for phonegap

--- WEBSITE/js/getDBTable.js
+++ WEBSITE/js/getDBTable.js
@@ -22,7 +22,7 @@ function getDBElement(dbTableName, nameOfElement, nameOfColumn, callback) {
             method: "POST",
             //dataType: "json",
             crossDomain: true,
-            url: "/php/getDBElement.php",
+            url: "https://hyper.arguggi.co.uk/php/getDBElement.php",
             data: {table: dbTableName, title: nameOfElement, column: nameOfColumn},
             success: function(response) {
                 callback(JSON.parse(response));
