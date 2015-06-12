bafa135be4fc9f363bc636ca7e7751c1224c5c4a
Arguggi <git@arguggi.co.uk>
2015-06-12 19:23:34 +0200

    Move getDBElement to getDBTable js
    Creating and including another js file in every page just for a single
    js function seems like a waste.

--- WEBSITE/js/getDBTable.js
+++ WEBSITE/js/getDBTable.js
@@ -15,3 +15,21 @@ function getDBTable(dbTableName, columnToOrder, callback) {
             }
     });
 }
+
+function getDBElement(dbTableName, nameOfElement, nameOfColumn, callback) {
+
+    $.ajax({
+            method: "POST",
+            //dataType: "json",
+            crossDomain: true,
+            url: "/php/getDBElement.php",
+            data: {table: dbTableName, title: nameOfElement, column: nameOfColumn},
+            success: function(response) {
+                callback(JSON.parse(response));
+            },
+            error: function(request,error)
+            {
+                callback(error);
+            }
+    });
+}
