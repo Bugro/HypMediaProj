60ee9c0d07943bf95ef710d25c91c086dd653b9c
Arguggi <git@arguggi.co.uk>
2015-06-14 15:51:35 +0200

    Add history page

---
+++ WEBSITE/history/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+</head>
+<body>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <div class="content">
+    <div class="container">
+<p>
+        <div class="center-text"><h1>Our History</h1></div>
+Founded in 1975 by brothers, William and Norman Dabish, the health club chain, which began as a single inner-city establishment in Milan, Italy, has grown into one of the most well-known and well respected health club organizations in the world.  The Powerhouse Gym name and brand has been one of the top leaders in the fitness industry for over 35 years.  With 300 licensees in 39 states, Powerhouse has continued to steadily gain global attention by expanding into 17 different countries worldwide.  The first Powerhouse Gym still stands in its original location and celebrated its 35th anniversary in 2010.
+
+Due to the overwhelming success of its original facilities and the demand that they created for additional out-of-state locations, a Licensing Division was formed in 1984. Today, as stated in the recent report published by the health and fitness advocacy organization IHRSA, the Powerhouse Gym Trademark and Service Marks are amongst the most recognized brands in the health and fitness industry.  Seven of every ten people in the United States recognize the Powerhouse Gym logo and/or insignia.
+
+With over 300 Licensee’s worldwide, the Powerhouse Gym name and logo provides its gyms instant recognition and credibility associated with an internationally registered trademark and service mark symbolic with quality in the health and fitness industry.
+
+Aside from having locations throughout North America, South America, Europe, India, Russia and Asia, the Powerhouse Gym name is supported by its popular and fashionable branded sportswear line.  Krystal Dabish has anchored the merchandising division for over 25 years.  Powerhouse Gym merchandising supplies sportswear and related items bearing the Powerhouse Gym name and logo to Powerhouse Gym locations and various retail channels throughout the world.  Powerhouse Gym sportswear is often worn by professional athletes and celebrities and can be seen featured in international sports and fitness publications, on television and in major motion picture productions.
+
+Not limiting itself to the obvious, Powerhouse Gym entered the media business in 1992 with its publication, Powerhouse Magazine. As an annual publication, Powerhouse Magazine has proved to be a powerful marketing tool for the Licensing Division as well as motivating potential members into the facilities themselves. Ten years later, Powerhouse TV, a personalized internal broadcast network was launched within Powerhouse Gyms across the United States creating a motivating workout environment with the ability to control advertisements, music and messages.
+
+The strength of Powerhouse Gyms International lies within in its licensee organization. Powerhouse Gym attracts entrepreneurs from all walks of life that meet together annually for the Powerhouse Gyms International Convention. This annual Powerhouse Gym Convention provides the organization an opportunity to share experiences, participate in a multitude of workshops and develop strategies to better service the membership and communities in which they operate.
+
+Licensing provides a blueprint for starting a business. A Powerhouse Licensee can get advice on where to locate, how to negotiate a lease, how much to charge depending on varying demographics, and receive operational advice. What sets Powerhouse apart from its competitors, is the fact that the founding family remains at the helm of the company. “In the end,” explains Will, “the good gyms are all very similar. What distinguishes them--the real difference--is service; service, equipment, and atmosphere is what differentiates Powerhouse Gym today. We pride ourselves on the service that we provide to our owners and their members. We’re here--we don’t delegate the job. We’re the original founding family, and you can’t buy or hire that kind of caring.”
+
+Today, Powerhouse Gyms International is in the midst of tremendous growth and expansion. One reason for this surge in membership and facilities has been Powerhouse Gym's ability to not only accommodate the conditioned market, but to also attract mainstream consumers and fitness enthusiast by providing excellent fitness facilities. The large muscular logo is a sign of credibility across the country and is a testament of where this company began. The Powerhouse Gym Trademark is associated with service and quality, and is apparent throughout each and every Powerhouse Gym that opens its doors to over 1.2 million dedicated members.
+
+An important component of this new growth is attributed to the energy of Henry Dabish, CEO in association with the experience of co-founder William Dabish and Krystal Dabish. Thirty-five years after the company was founded, Powerhouse Gyms International is comprised of over 300 Licensee’s in 39 states and 17 countries. In North America, Powerhouse continues to grow steadily with its well established roots in the fitness industry. The largest clusters of growth and development lately have occurred overseas, making Powerhouse Gym a true international Powerhouse.
+<p>
+
+
+    </div>
+  </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+</body>
+</html>
