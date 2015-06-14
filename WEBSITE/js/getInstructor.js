1fc91dbd22ebb536f7cc434904dc8b06a8fc5540
Arguggi <git@arguggi.co.uk>
2015-06-14 19:09:23 +0200

    Instructor relative path

--- WEBSITE/js/getInstructor.js
+++ WEBSITE/js/getInstructor.js
@@ -6,19 +6,11 @@ $( document ).ready(function() {
         } else {
             var instructorList="";
             for(var i=0;i<instructor.length;i++){
-                $("#instructorList").append("<div id='"+instructor[i].name.split(' ')[0]+"'><div class='col-md-4'><div class='small-overlay-img'><a href='../instructors/oneInstructor.html?"+instructor[i].name+"'><img class='small-img img-rounded' src='/assets/instruc-"+instructor[i].name+".jpg'><p class='desc white-links'>"+instructor[i].name+"</p></a></div></div></div>");
-               $("#"+instructor[i].name.split(' ')[0]+"").on("click", {title: instructor[i].name, description: instructor[i].description, speciality: instructor[i].speciality}, displayDescription);
-                console.log(instructor[i].speciality);
+                $("#instructorList").append("<div id='"+instructor[i].name.split(' ')[0]+"'><div class='col-md-4'><div class='small-overlay-img'><a href='../instructors/oneInstructor.html?"+instructor[i].name+"'><img class='small-img img-rounded' src='../assets/instruc-"+instructor[i].name+".jpg'><p class='desc white-links'>"+instructor[i].name+"</p></a></div></div></div>");
             }
         }
     });
 });
-
-
-function displayDescription(event) {
-    instructorClicked = event.data.title;
-    window.location.href = "../instructors/oneInstructor.html?"+instructorClicked;
-}
 
 function displayCourse(event) {
     $("#instructorList").html("");
@@ -29,7 +21,7 @@ function displayCourse(event) {
             var courseList="";
             for(var i=0;i<course.length;i++){
                 if(course[i].title == event.data.title){
-                    elmt = "<h3 class='center-text' id='courseTitle'>"+course[i].title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='/assets/"+course[i].title+".jpg'></div></div><div class='col-md-9' id='courseDescription'><p>"+course[i].description+"</p></div></div><a href='/instructors/' class='btn btn-primary btn-room' role='button'>Back</a>";
+                    elmt = "<h3 class='center-text' id='courseTitle'>"+course[i].title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='../assets/"+course[i].title+".jpg'></div></div><div class='col-md-9' id='courseDescription'><p>"+course[i].description+"</p></div></div><a href='../instructors/' class='btn btn-primary btn-room' role='button'>Back</a>";
         $("#instructorList").append(elmt);
                 }
             }
