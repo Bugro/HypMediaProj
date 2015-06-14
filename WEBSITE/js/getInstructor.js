55ccac872f639cca2d8da2fcd9fabb7182431104
Bugro <guillaume.burgaud@free.fr>
2015-06-14 18:07:20 +0200

    Instructor should work now

--- WEBSITE/js/getInstructor.js
+++ WEBSITE/js/getInstructor.js
@@ -6,12 +6,19 @@ $( document ).ready(function() {
         } else {
             var instructorList="";
             for(var i=0;i<instructor.length;i++){
-                var name = instructor[i].name.split(' ')[0];
-                $("#instructorList").append("<div id='"+name+"'><div class='col-md-4'><div class='small-overlay-img'><a href='../instructors/oneInstructor.html?"+name+"'><img class='small-img img-rounded' src='../assets/instruc-"+name+".jpg'><p class='desc white-links'>"+name+"</p></img></a></div></div></div>");
+                $("#instructorList").append("<div id='"+instructor[i].name.split(' ')[0]+"'><div class='col-md-4'><div class='small-overlay-img'><img class='small-img img-rounded' src='/assets/instruc-"+instructor[i].name+".jpg'><p class='desc white-links'>"+instructor[i].name+"</p></div></div></div>");
+               $("#"+instructor[i].name.split(' ')[0]+"").on("click", {title: instructor[i].name, description: instructor[i].description, speciality: instructor[i].speciality}, displayDescription);
+                console.log(instructor[i].speciality);
             }
         }
     });
 });
+
+
+function displayDescription(event) {
+    instructorClicked = event.data.title;
+    window.location.href = "../instructors/oneInstructor.html?"+instructorClicked;
+}
 
 function displayCourse(event) {
     $("#instructorList").html("");
@@ -22,7 +29,7 @@ function displayCourse(event) {
             var courseList="";
             for(var i=0;i<course.length;i++){
                 if(course[i].title == event.data.title){
-                    elmt = "<h3 class='center-text' id='courseTitle'>"+course[i].title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='../assets/"+course[i].title+".jpg'></div></div><div class='col-md-9' id='courseDescription'><p>"+course[i].description+"</p></div></div><a href='../instructors/index.html' class='btn btn-primary btn-room' role='button'>Back</a>";
+                    elmt = "<h3 class='center-text' id='courseTitle'>"+course[i].title+"</h3><div class='row'><div class='col-md-3'><div class='small-overlay-img'><img class='img-equip img-rounded' src='/assets/"+course[i].title+".jpg'></div></div><div class='col-md-9' id='courseDescription'><p>"+course[i].description+"</p></div></div><a href='/instructors/' class='btn btn-primary btn-room' role='button'>Back</a>";
         $("#instructorList").append(elmt);
                 }
             }
