682f1e4eceda36d4166f4c9fb237cdfe4b35422e
Arguggi <git@arguggi.co.uk>
2015-06-17 16:40:25 +0200

    Revert "Remove Our Gym Page"
    This reverts commit 80976c48ea46ee90f52ba8d4dbbfc1a50310c37c.

---
+++ WEBSITE/gym/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+</head>
+<body>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <div class="content">
+    <div class="container">
+      <h1 class="center-text">Welcome to the Gym</h1><img class="center-img img-rounded" src="/assets/gym.jpg">
+      <div class="row">
+        <div class="col-md-3">
+          <a href="/history/" class="btn btn-primary btn-room" role="button">History</a>
+        </div>
+        <div class="col-md-3">
+          <a href="/location/" class="btn btn-primary btn-room" role="button">Location</a>
+        </div>
+        <div class="col-md-3">
+          <a href="/testimonials/" class="btn btn-primary btn-room" role="button">Testimonials</a>
+        </div>
+        <div class="col-md-3">
+          <a href="/Equipment/" class="btn btn-primary btn-room" role="button">Equipment</a>
+        </div>
+      </div>
+    </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+  </div>
+</body>
+</html>
