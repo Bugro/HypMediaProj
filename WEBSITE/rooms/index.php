a7749a9e127060dc0e35e156bd878e9b99e80c2e
Arguggi <git@arguggi.co.uk>
2015-06-17 16:53:24 +0200

    Add selection page

--- WEBSITE/rooms/index.php
+++ WEBSITE/rooms/index.php
@@ -10,6 +10,7 @@
 	<div class="center-text"><h1>Interactive map</h1></div>
         <div id="mapOfTheGym"><img id="imgmap" class="img-rounded" src="/assets/gymplan.jpg"></div>
         <div class="selectedRoom">
+      <a href="/rooms/index.php" class="btn btn-primary btn-room" role="button">All rooms</a>
             <div class="center-text"><h1>Selected Room</h1></div>
             <div class="descriptionOfRoom">Here goes the description of the room and all the information on its location, what people are doing inside...</div>
         </div>
@@ -17,3 +18,5 @@
   </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
 </body>
 </html>
+
+
