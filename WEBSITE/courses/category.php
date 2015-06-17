5db06e483bccda63a43b2357a74b3b46e7eb99e1
Arguggi <git@arguggi.co.uk>
2015-06-17 14:37:04 +0200

    Add course category

---
+++ WEBSITE/courses/category.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+  <title></title>
+</head>
+<body>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
+  <div class="content">
+    <div class="container">
+      <div class="row bottom-padding">
+        <div class="col-md-3">
+          <a href="category.php" class="btn btn-primary btn-room" role="button">All course categories</a>
+        </div>
+        <div class="col-md-9">
+        <h3 class="center-text">Yoga</h3>
+          <div class="row">
+            <div class="col-md-3"><img class="center-img img-rounded img-equip" src="/assets/course-hatha.jpg"></div>
+            <div class="col-md-9">
+              <p> Derived from the Sanskrit word yuj, Yoga means union of the individual consciousness or soul with the Universal Consciousness or Spirit. Yoga is a 5000 year old Indian body of knowledge. Though many think of yoga only as a physical exercise where people twist, turn, stretch, and breathe in the most complex ways, these are actually only the most superficial aspect of this profound science of unfolding the infinite potentials of the human mind and soul.
+The science of Yoga imbibe itself the complete essence of the Way of Life, including - Gyan Yoga or philosophy, Bhakti Yoga or path of devotional bliss, Karma Yoga or path of blissful action, and Raja Yoga or path of mind control. Raja Yoga is further divided into eight parts. At the heart of the Raja Yoga system, balancing and unifying these various approaches, is the practice of Yoga.</p>
+              <p class="center-text">Why it's good:</p>
+              <p class="center-text">For who:</p>
+              <p class="center-text">Taught by: <a href="../instructors/index.php">Amy</a></p>
+              <p class="center-text">Courses: <a href="../courses/index.php">Hatha Yoga</a></p>
+            </div>
+          </div>
+          <h3 class="center-text">Combat</h3>
+            <div class="row">
+              <div class="col-md-3"><img class="center-img img-rounded img-equip" src="/assets/course-boxing.jpg"></div>
+              <div class="col-md-9">
+                <p>Boxing is often labeled as old-school in nature. The perseverance and mental fortitude required from a successful fighter is unique from other sports. When a boxer demonstrates courage and tenacity inside the ring, he is often labeled as a "throwback" to the golden days of boxing.</p>
+                <p class="center-text">Origins:</p>
+                <p class="center-text">Why it's good:</p>
+                <p class="center-text">For who:</p>
+                <p class="center-text">Taught by: <a href="../instructors/index.php">Adama</a></p>
+                <p class="center-text">Courses: <a href="../courses/index.php">Boxing</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+    </div>
+  </div>
+</body>
+</html>
