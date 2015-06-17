b34628bf7138610bc7043e724f837156744534aa
Arguggi <git@arguggi.co.uk>
2015-06-17 14:25:05 +0200

    Instructors follow pidm

--- WEBSITE/instructors/index.php
+++ WEBSITE/instructors/index.php
@@ -2,55 +2,43 @@
 <html lang="en">
 <head>
   <?php include($_SERVER['DOCUMENT_ROOT'].'/head.html'); ?>
+  <title></title>
 </head>
 <body>
   <?php include($_SERVER['DOCUMENT_ROOT'].'/header.html'); ?>
   <div class="content">
     <div class="container">
-      <h2 class="center-text">Personal Fitness Trainers</h2>
-      <div class="row">
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-amy.jpg">
-            <p class="desc white-links">AMY</p>
+      <div class="row bottom-padding">
+        <div class="col-md-3">
+          <a href="/courses/" class="btn btn-primary btn-room" role="button">All instructors</a> <a href="/courses/" class="btn btn-success btn-room" role="button">Instructor of the month</a>
+        </div>
+        <div class="col-md-9">
+        <h3 class="center-text">Adam</h3>
+          <div class="row">
+            <div class="col-md-3"><img class="center-img img-rounded img-equip" src="/assets/instruc-adam.jpg"></div>
+            <div class="col-md-9">
+              <p>Adam is a Fitness and Conditioning trainer focusing on the fundamentals of Boxing and Muay Thai. He first started training in combative arts in 1994 and competed in several tournaments in the Los Angeles area. He also trained in MMA at the Piccinini Training Center, 228th Street MMA Gym and Blackhouse.</p>
+              <p class="center-text">Qualification:</p>
+              <p class="center-text">Awards:</p>
+              <p class="center-text">Teaches Catagory: <a href="../courses/category.php">Combat</a></p>
+              <p class="center-text">Teaches Course: <a href="../courses/index.php">Boxing</a></p>
+            </div>
+          </div>
+          <h3 class="center-text">Amy</h3>
+            <div class="row">
+              <div class="col-md-3"><img class="center-img img-rounded img-equip" src="/assets/instruc-amy.jpg"></div>
+              <div class="col-md-9">
+                <p>I have a dance background of Jazz, Ballet Contemporary dance and now, Cardio Street Funk fitness. I love teaching Cardio Street Funk Fitness classes. I combine different styles of dance to create a unique blend of fun calorie burning fitness choreography that will feel more like a party or a night out dancing than an actual workout. Come try it, I guarantee you will have a blast! I encourage you to make an investment in your health by incorporating daily exercise in your life. No dance experience required!! Studies have shown, people that exercise, live a longer life. Having said that, I invite you to focus on living a healthier and longer life. Forget the workout. Join the Cardio Street Funk Party!!, See you in class, Woot Woot!!!</p>
+                <p class="center-text">Qualification:</p>
+                <p class="center-text">Awards:</p>
+                <p class="center-text">Teaches Catagory: <a href="../courses/category.php">Yoga</a></p>
+                <p class="center-text">Teaches Course: <a href="../courses/index.php">Hatha Yoga</a></p>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-katie.jpg">
-            <p class="desc white-links">KATIE</p>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-mike.jpg">
-            <p class="desc white-links">MIKE</p>
-          </div>
-        </div>
-      </div>
-      <h2 class="center-text">Boxing/Kickbox Trainer</h2>
-      <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-adam.jpg">
-            <p class="desc white-links">ADAM</p>
-          </div>
-        </div>
-        <div class="col-md-4"></div>
-      </div>
-      <h2 class="center-text">Pilates / Yoga Trainer</h2>
-      <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-          <div class="overlay-img">
-            <img class="body-img img-rounded" src="/assets/instruc-mia.jpg">
-            <p class="desc white-links">MIA</p>
-          </div>
-        </div>
-        <div class="col-md-4"></div>
-      </div>
+      </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
     </div>
-  </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+  </div>
 </body>
 </html>
