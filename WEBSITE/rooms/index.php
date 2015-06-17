a58cbd17bbd448638873f5dc737061126676d0e6
Arguggi <git@arguggi.co.uk>
2015-06-17 17:44:53 +0200

    Change room page

--- WEBSITE/rooms/index.php
+++ WEBSITE/rooms/index.php
@@ -13,6 +13,20 @@
             <div class="center-text"><h1>Selected Room</h1></div>
             <div class="descriptionOfRoom">Here goes the description of the room and all the information on its location, what people are doing inside...</div>
         </div>
+      <div class="row bottom-padding">
+        <h1 class="center-text"> Small Room</h1>
+        <div class="col-md-3"><img class="center-img img-rounded img-equip" src="/assets/small.jpg"></div>
+        <div class="col-md-9">
+          <p>Courses held here: <a href="../courses/index.php">Boxing</a></p>
+      </div>
+    </div>
+    <div class="row bottom-padding">
+        <h1 class="center-text"> Big Room</h1>
+        <div class="col-md-3"><img class="center-img img-rounded img-equip" src="/assets/medium.jpg"></div>
+        <div class="col-md-9">
+          <p>Courses held here: <a href="../courses/index.php">Hatha Yoga</a></p>
+      </div>
+    </div>
     </div>
   </div><?php include($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
 </body>
